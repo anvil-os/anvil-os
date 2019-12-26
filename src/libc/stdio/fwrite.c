ac17355a1fd0596cf97ade50d5aaab1c8e0f2b36
Gerard Green <gerryg400@gmail.com>
2019-12-26 17:49:54 +1100

    Added fwrite

--- src/libc/stdio/fwrite.c
+++ src/libc/stdio/fwrite.c
@@ -3,5 +3,33 @@
 
 size_t fwrite(const void *restrict ptr, size_t size, size_t nmemb, FILE *restrict stream)
 {
-    return -1;
+    const unsigned char *p = ptr;
+    size_t bytes;
+
+    if (size == 0 || nmemb == 0)
+    {
+        // Nothing to write so we are done
+        return 0;
+    }
+
+    bytes = size * nmemb;
+    if (bytes < size || bytes < nmemb)
+    {
+        // Overflow
+        return 0;
+    }
+
+    _Anvil_flockfile(stream);
+    while (bytes)
+    {
+        if (_Putc_unlocked(*p, stream) == EOF)
+        {
+            break;
+        }
+        ++p;
+        --bytes;
+    }
+    _Anvil_funlockfile(stream);
+
+    return (p - (const unsigned char *)ptr) / size;;
 }
