42bd18bc29630886b35e013fdf02b7b95babf9ec
Gerard Green <gerryg400@gmail.com>
2019-12-26 22:29:29 +1100

    Added abs and friends

---
+++ src/libc/stdlib/labs.c
@@ -0,0 +1,7 @@
+
+#include <stdlib.h>
+
+long int labs(long int j)
+{
+    return j < 0 ? -j : j;
+}
---
+++ src/libc/stdlib/llabs.c
@@ -0,0 +1,7 @@
+
+#include <stdlib.h>
+
+long long int llabs(long long int j)
+{
+    return j < 0 ? -j : j;
+}
