f5604c4a7c014dd6e756453e4f38228860de9942
Gerard Green <gerryg400@gmail.com>
2020-02-01 14:35:31 +1100

    Moved test data to a separate file

---
+++ src/libc/test/double_test_data.h
@@ -0,0 +1,18 @@
+
+#ifndef TEST_DATA_H
+#define TEST_DATA_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+struct double_test_data_s
+{
+    const char *str;
+    uint32_t high;
+    uint32_t low;
+};
+
+extern struct double_test_data_s base_10_parser_data[];
+extern struct double_test_data_s gdtoa_test_data[];
+
+#endif /* TEST_DATA_H */
